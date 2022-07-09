5773c0828c2891a376d9edaf3ed7a20a7f3c3489
Catsher <iluha_m_74@mail.ru>
2022-07-09 13:48:19 +0500

    Made some generators
    1)Made generators: user, test, question
    2) Commented not usable classes

--- Employees/Employee.py
+++ Employees/Employee.py
@@ -1,6 +1,9 @@
-class Employee:
-    def __init__(self, name, password, job_title, completed_tests):
-        self.name = name
-        self.password = password
-        self.job_title = job_title
-        self.completed_tests = completed_tests
+# class Employee:
+#     def __init__(self, name, password, job_title, completed_tests):
+#         self.name = name
+#         self.password = password
+#         self.job_title = job_title
+#         self.completed_tests = completed_tests
+
+def create_new_user(name, password, job_title):
+    return [0, name, password, job_title, []]
