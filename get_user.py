4389460cfea8cbab0e0313b9c09524ff5bd24181
Catsher <iluha_m_74@mail.ru>
2022-07-17 21:13:00 +0500

    Added Admin_view_tests_and_users
    Still in progress

--- get_user.py
+++ get_user.py
@@ -1,4 +1,4 @@
 def get_user(user_login_password):
     # getting user by his login and password
     # If there is no user with this login and password - return None
-    return []
+    return {"name": "Sanya"}
