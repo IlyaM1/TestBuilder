708440ab143205b682770cd96c6f8012577badef
Catsher <iluha_m_74@mail.ru>
2022-07-20 23:27:47 +0500

    Added TabWidget
    Added TabWidget with two tabs: test_page and user_page

--- Interface/Admin_view_tests_and_users.py
+++ Interface/Admin_view_tests_and_users.py
@@ -1,11 +1,38 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QApplication, QTabWidget, QVBoxLayout
+
 
 class Admin_view_tests_and_users(QWidget):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super(QWidget, self).__init__(parent)
         self.init_UI()
 
     def init_UI(self):
-        pass
+        self.setMinimumSize(1280, 720)
 
+        self.container = QVBoxLayout(self)
+
+        self.tab_widget = QTabWidget()
+
+        self.user_page = self.init_users_page()
+        self.test_page = self.init_tests_page()
+
+        self.tab_widget.addTab(self.test_page, "Тесты")
+        self.tab_widget.addTab(self.user_page, "Сотрудники")
+
+        self.container.addWidget(self.tab_widget)
+        self.show()
+
+
+
+    def init_users_page(self):
+        return QWidget()
+
+    def init_tests_page(self):
+        return QWidget()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    auth_obj = Admin_view_tests_and_users()
+    app.exec_()
