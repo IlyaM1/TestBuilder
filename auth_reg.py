3eb62e17214ac2dc523f73a86a8d958b72af2469
akrisfx <vpotach@gmail.com>
2022-08-09 17:06:07 +0300

    added auth and registration

--- auth_reg.py
+++ auth_reg.py
@@ -1,7 +1,6 @@
 from db.hash import hash_password
 
-
-inputFromFront = {'name': 'Egor Lob',
+inputFromFront = {'id': 0, 'name': 'Egor Lob',
                   'password': '123'}  # к нам приходит инфа с фронта, а мы, имея эту инфу пишем под модуль из визио функции
 
 
@@ -13,10 +12,17 @@ class Signing:
         self.name = auth_info["name"]
         self.password = auth_info["password"]
         self.password = hash_password(self.password)
+        auth_info["password"] = self.password
         self.s = SQLInteract_obj
 
     def registration(self):
+        """если возвратила False, значит рега не сработала"""
         auth_info_arr = [self.auth_info[x] for x in self.auth_info]
-        self.s.sql_add_new_user(auth_info_arr)
+        add_check = self.s.sql_add_new_user(auth_info_arr)
         print(self.s.sql_get_user_with_id(auth_info_arr[0]))
+        return add_check
 
+    def authentication(self):
+        """если возвратила False, значит такого юзера нету, данные введены неверно"""
+        user = self.s.sql_get_user_with_namePass(name=self.name, password=self.password)
+        return user
