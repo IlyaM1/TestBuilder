fff0c57ff4d7f2b28eb8025daaeae2e3fac996b9
akrisfx <vpotach@gmail.com>
2022-08-10 13:58:17 +0300

    change user dict

--- Interface/Authorization.py
+++ Interface/Authorization.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel, QLineEdit
 from Interface.View_all_tests import View_all_tests
 from test_data_funcs import get_all_tests, get_users
+from auth_reg import Signing
+
 
 class Authorization(QWidget):
 
@@ -29,16 +31,19 @@ class Authorization(QWidget):
         button_login.clicked.connect(self.button_login_pushed)
         vertical_layout.addWidget(button_login)
 
-
         self.setLayout(vertical_layout)
         self.show()
 
     def button_login_pushed(self):
         self.user_login_password = (self.input_label_login.text(), self.input_label_password.text())
         # user = get_user(self.user_login_password)
-        user = {"name": "Vlad"}
+        user = {"id": 0, "name": self.user_login_password[0], "password": self.user_login_password[1], "post": "",
+                "tests": '[]'}
         if user is not None:
             self.close()
             test = get_all_tests(user)[0]
             tests = [test for i in range(20)]
             self.view_all_tests = View_all_tests(tests, user)
+
+    def authorization(self):
+        pass
