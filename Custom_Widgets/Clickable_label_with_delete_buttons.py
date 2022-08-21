962d30d52858acfcffec4c4aa7e6fea547c66438
Catsher <iluha_m_74@mail.ru>
2022-08-21 23:05:24 +0500

    Connected all Admin widgets
    1)
    1. Connected Authorization with Admin_view_tests_and_users (made TODO: вызывать некст окно с передачей user_arr и test_arr)
    2. Connected Admin_view_tests_and_users with Admin_test_view and Admin_user_view
    2) Now Clickable_label_with_delete_buttons contains dictionary, NOT id
    3) Fixed self.EMPTY_QUESTION bug(this not changes now after generating new question)
    4) Changed all paths to css files with absolute path
    5) create_new_test/user now creates empty test/user(should also add new test/user in db)
    6) Fixed signing as admin

--- Custom_Widgets/Clickable_label_with_delete_buttons.py
+++ Custom_Widgets/Clickable_label_with_delete_buttons.py
@@ -6,13 +6,13 @@ from Custom_Widgets.Clickable_label import Clickable_label
 from config import Config
 class Clickable_label_with_delete_buttons(QLabel):
     """
-    QLabel на который можно нажать, который хранит в себе id объекта для которого создан, также имеет контекстное меню из трёх действий: посмотреть детальную информацию(?), изменить, удалить
+    QLabel на который можно нажать, который хранит в себе dictionary объекта для которого создан, также имеет контекстное меню из трёх действий: посмотреть детальную информацию(?), изменить, удалить
     """
 
-    def __init__(self, text, id, icon_see_path=Config().config["path"] + "\\Interface\\see_icon.png", icon_edit_path=Config().config["path"] + "\\Interface\\edit_icon.png", icon_delete_path=Config().config["path"] + "\\Interface\\Trash_Bin.png"):
+    def __init__(self, text, dictionary, icon_see_path=Config().config["path"] + "\\Interface\\see_icon.png", icon_edit_path=Config().config["path"] + "\\Interface\\edit_icon.png", icon_delete_path=Config().config["path"] + "\\Interface\\Trash_Bin.png"):
         super().__init__()
         self.text = text
-        self.id = id
+        self.dictionary = dictionary
         self.icon_see_path = icon_see_path
         self.icon_edit_path = icon_edit_path
         self.icon_delete_path = icon_delete_path
@@ -21,7 +21,7 @@ class Clickable_label_with_delete_buttons(QLabel):
     def init_UI(self):
         self.main_horizontal_layout = QHBoxLayout()
 
-        self.label = Clickable_label(self.text, self.id)
+        self.label = Clickable_label(self.text, self.dictionary["id"])
         self.main_horizontal_layout.addWidget(self.label)
 
         self.widget_for_layout_for_buttons = QWidget()
