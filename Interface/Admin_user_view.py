a702a6d736c237de67bb345619bc7284295b3986
smasher <iluha_m_74@mail.ru>
2023-01-19 23:38:20 +0500

    Refactored widget and fixed save button 1) Refactoring:        Generating "big" widgets functions:            1) made generate_constant_widget            2) made generate_solved_test_widget            3) fixed init_layout_of_solved_test            4) fixed init_question_widget        Generating "small" widgets functions:            1) made generate_label_add_to_widget            2) made generate_button_with_slot            3) made generate_and_add_label_with_input            4) made generate_and_add_label        Other:            1) Made set_css function            2) Optimized init_UI function 2) Now user can press "Save" button without scrolling all solved tests

--- Interface/Admin_user_view.py
+++ Interface/Admin_user_view.py
@@ -20,70 +20,106 @@ class Admin_user_view(QMainWindow):
 
     def init_UI(self):
         self.setMinimumSize(1280, 720)
+        self.set_css()
 
-        with open(Config().config["path"] + '\\Interface\\css\\Admin_user_view.css') as css:
-            self.setStyleSheet(css.read())
-
-
-        self.central_widget = QWidget()
         self.container = QVBoxLayout(self)
 
-        self.constant_widget = QWidget()
-        self.constant_widget_layout = QVBoxLayout()
-
-        self.name_input_label = self.generate_and_add_label_with_input("Имя сотрудника: ", self.user["name"], self.constant_widget_layout)
-
-        self.password_input_label = self.generate_and_add_label_with_input("Пароль сотрудника: ", self.user["password"], self.constant_widget_layout)
-        self.password_input_label.setEchoMode(QLineEdit.Password)
-
-        self.post_input_label = self.generate_and_add_label_with_input("Должность сотрудника: ", self.user["post"], self.constant_widget_layout)
-
-        self.constant_widget.setLayout(self.constant_widget_layout)
+        self.constant_widget = self.generate_constant_widget()
         self.container.addWidget(self.constant_widget)
 
-        if self.user["tests"] not in [None, False, []]:
-            self.solved_test_widget = QWidget()
-            self.solved_test_layout = QVBoxLayout()
+        self.solved_test_widget = self.generate_solved_test_widget(self.user["tests"])
+        self.container.addWidget(self.solved_test_widget)
 
-            self.solved_test_label = QLabel("Решённые тесты: ")
-            self.solved_test_layout.addWidget(self.solved_test_label)
-
-            for i in self.user["tests"]:
-                self.solved_test_layout.addWidget(self.init_layout_of_solved_test(i))
-
-            self.solved_test_widget.setLayout(self.solved_test_layout)
-            self.container.addWidget(self.solved_test_widget)
-        else:
-            self.solved_test_widget = QWidget()
-            self.solved_test_layout = QVBoxLayout()
-
-            self.solved_test_label = QLabel("Решённые тесты отсутствуют")
-            self.solved_test_layout.addWidget(self.solved_test_label)
-            self.solved_test_widget.setLayout(self.solved_test_layout)
-            self.container.addWidget(self.solved_test_widget)
-
-        self.save_user_button = QPushButton("Сохранить")
-        self.save_user_button.clicked.connect(self.save_user_button_pushed)
+        self.save_user_button = self.generate_button_with_slot("Сохранить", self.save_user_button_pushed)
         self.container.addWidget(self.save_user_button)
 
+        self.central_widget = QWidget()
         self.central_widget.setLayout(self.container)
 
         scroll_widget = self.generate_scroll_widget(self.central_widget)
-
         self.setCentralWidget(scroll_widget)
+
         self.show()
 
-    @staticmethod
-    def generate_and_add_label_with_input(label_text, input_text, parent_widget):
-        """
-        Returns input widget
-        """
-        label = QLabel(label_text)
-        parent_widget.addWidget(label)
-        input = QLineEdit(input_text)
-        parent_widget.addWidget(input)
+    def set_css(self):
+        with open(Config().config["path"] + '\\Interface\\css\\Admin_user_view.css') as css:
+            self.setStyleSheet(css.read())
 
-        return input
+    def generate_constant_widget(self):
+        constant_widget = QWidget()
+        constant_widget_layout = QVBoxLayout()
+
+        self.name_input_label = self.generate_and_add_label_with_input("Имя сотрудника: ", self.user["name"],
+                                                                       constant_widget_layout)
+        self.password_input_label = self.generate_and_add_label_with_input("Пароль сотрудника: ", self.user["password"],
+                                                                           constant_widget_layout)
+        self.password_input_label.setEchoMode(QLineEdit.Password)
+        self.post_input_label = self.generate_and_add_label_with_input("Должность сотрудника: ", self.user["post"],
+                                                                       constant_widget_layout)
+
+        constant_widget.setLayout(constant_widget_layout)
+        return constant_widget
+
+    def generate_solved_test_widget(self, tests):
+        solved_test_widget = QWidget()
+        solved_test_layout = QVBoxLayout()
+
+        if tests not in [None, False, []]:
+            solved_test_label = QLabel("Решённые тесты: ")
+            solved_test_layout.addWidget(solved_test_label)
+
+            for test in tests:
+                solved_test_layout.addWidget(self.init_layout_of_solved_test(test))
+
+            solved_test_widget.setLayout(solved_test_layout)
+        else:
+            solved_test_label = QLabel("Решённые тесты отсутствуют")
+            solved_test_layout.addWidget(solved_test_label)
+            solved_test_widget.setLayout(solved_test_layout)
+
+        solved_test_scroll_area = Admin_user_view.generate_scroll_widget(solved_test_widget)
+        return solved_test_scroll_area
+
+    @staticmethod
+    def init_layout_of_solved_test(test):
+        test_widget_layout = QVBoxLayout()
+        Admin_user_view.generate_label_add_to_widget(f'Имя теста: {test["name"]}', test_widget_layout)
+        Admin_user_view.generate_label_add_to_widget(f'Тема теста: {test["theme"]}', test_widget_layout)
+        Admin_user_view.generate_label_add_to_widget(f'Результат теста: {test["result"]}/{test["max_result"]}', test_widget_layout)
+
+        box_for_wrong_questions = CollapsibleBox(title="Вопросы: ")
+        box_for_wrong_questions_layout = QVBoxLayout()
+
+        for question in test["test"]:
+            if question["answer"] != question["key"] or True:
+                box_for_wrong_questions_layout.addWidget(Admin_user_view.init_question_widget(question))
+
+        box_for_wrong_questions_layout.setAlignment(Qt.AlignTop)
+        box_for_wrong_questions.setContentLayout(box_for_wrong_questions_layout)
+        test_widget_layout.addWidget(box_for_wrong_questions)
+
+        test_widget = QWidget()
+        test_widget.setLayout(test_widget_layout)
+        return test_widget
+
+    @staticmethod
+    def init_question_widget(question):
+        solved_test_question_widget = QWidget()
+        solved_test_question_widget_layout = QVBoxLayout()
+
+        solved_test_question_label = QLabel(f'Вопрос: {question["question"]}')
+        solved_test_question_widget_layout.addWidget(solved_test_question_label)
+
+        solved_test_answer_label = QLabel(f'Ответ/Правильный ответ: {question["answer"]}/{question["key"]}')
+        solved_test_question_widget_layout.addWidget(solved_test_answer_label)
+
+        solved_test_question_widget.setLayout(solved_test_question_widget_layout)
+        return solved_test_question_widget
+
+    @staticmethod
+    def generate_label_add_to_widget(text, parent_widget):
+        label = QLabel(text)
+        parent_widget.addWidget(label)
 
     @staticmethod
     def generate_scroll_widget(widget_to_set):
@@ -95,45 +131,26 @@ class Admin_user_view(QMainWindow):
 
         return scroll_widget
 
-    def init_layout_of_solved_test(self, test):
-        self.test_widget = QWidget()
+    @staticmethod
+    def generate_button_with_slot(text, connect_function):
+        button = QPushButton(text)
+        button.clicked.connect(connect_function)
 
-        self.test_widget_layout = QVBoxLayout()
+        return button
 
-        self.name_label = QLabel(f'Имя теста: {test["name"]}')
-        self.test_widget_layout.addWidget(self.name_label)
+    @staticmethod
+    def generate_and_add_label_with_input(label_text, input_text, parent_widget):
+        label = QLabel(label_text)
+        parent_widget.addWidget(label)
+        input = QLineEdit(input_text)
+        parent_widget.addWidget(input)
 
-        self.theme_label = QLabel(f'Тема теста: {test["theme"]}')
-        self.test_widget_layout.addWidget(self.theme_label)
+        return input
 
-        self.result_label = QLabel(f'Результат теста: {test["result"]}/{test["max_result"]}')
-        self.test_widget_layout.addWidget(self.result_label)
-
-        self.box_for_wrong_questions = CollapsibleBox(title="Вопросы: ")
-        self.box_for_wrong_questions_layout = QVBoxLayout()
-        for i in test["questions"]:
-            if i["answer"] != i["key"] or True:
-                self.box_for_wrong_questions_layout.addWidget(self.init_question_widget(i))
-        self.box_for_wrong_questions_layout.setAlignment(Qt.AlignTop)
-        self.box_for_wrong_questions.setContentLayout(self.box_for_wrong_questions_layout)
-        self.test_widget_layout.addWidget(self.box_for_wrong_questions)
-
-        self.test_widget.setLayout(self.test_widget_layout)
-        return self.test_widget
-
-    def init_question_widget(self, question):
-        self.solved_test_question_widget = QWidget()
-        self.solved_test_question_widget_layout = QVBoxLayout()
-
-        self.solved_test_question_label = QLabel(f'Вопрос: {question["question"]}')
-        self.solved_test_question_widget_layout.addWidget(self.solved_test_question_label)
-
-        self.solved_test_answer_label = QLabel(f'Ответ/Правильный ответ: {question["answer"]}/{question["key"]}')
-        self.solved_test_question_widget_layout.addWidget(self.solved_test_answer_label)
-
-        self.solved_test_question_widget.setLayout(self.solved_test_question_widget_layout)
-
-        return self.solved_test_question_widget
+    @staticmethod
+    def generate_and_add_label(text, parent_widget):
+        name_label = QLabel(f'Имя теста: {text}')
+        parent_widget.addWidget(name_label)
 
     def save_user_button_pushed(self):
         input_name = self.name_input_label.text().strip()
@@ -158,8 +175,8 @@ if __name__ == '__main__':
     test_db = SQLInteract(table_name='testcase', filename_db=cfg.config["path"] + '/db/users.db')
     app = QApplication([])
     user = get_users()[0]
-#     print(test_db.return_full_table(to_dict=True))
-#     user = test_db.sql_get_user_with_id(2)
+    #     print(test_db.return_full_table(to_dict=True))
+    #     user = test_db.sql_get_user_with_id(2)
     # test_db.sql_update_one_by_id(update_field="password", update_value="ersg", search_id=2)
     # print(test_db.return_full_table())
     # print(user)
