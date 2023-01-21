9aecf927f51790bd6b0e9d40d082e36caf36deed
smasher <iluha_m_74@mail.ru>
2023-01-21 21:24:21 +0500

    Made gradient for progress bar in Ending_test_widget

--- Interface/Ending_test_widget.py
+++ Interface/Ending_test_widget.py
@@ -6,13 +6,15 @@ class Ending_test_widget(QWidget):
     def __init__(self, result=0, wrong_answers=0, max_result=0, user={}, parent=None):
         super(QWidget, self).__init__(parent)
         self.result = result
-        self.wrong_answers = wrong_answers
         self.max_result = max_result
         self.user = user
         self.init_UI()
 
     def init_UI(self):
         self.setMinimumSize(400, 300)
+
+        with open(cfg().config["path"] + '/Interface/css/Ending_test_widget.css', "r") as css:
+            self.setStyleSheet(css.read())
 
         self.main_layout = QVBoxLayout()
         self.setLayout(self.main_layout)
@@ -27,7 +29,12 @@ class Ending_test_widget(QWidget):
 
         # Create a progress bar to visualize the result
         self.result_progress = QProgressBar()
-        self.result_progress.setValue(int((self.result / self.max_result) * 100))
+        percent_result = int((self.result / self.max_result) * 100)
+        self.result_progress.setValue(percent_result)
+        self.result_progress.setStyleSheet(
+            "QProgressBar::chunk *background-color: rgb({0}, {1}, 0)?".format(int(255 / 100 * (100 - percent_result)),
+                                                                              int(255 / 100 * percent_result)).replace(
+                '*', '{').replace('?', '}'))
         result_layout.addWidget(self.result_progress)
 
         # Create a button to close the widget
@@ -35,13 +42,10 @@ class Ending_test_widget(QWidget):
         self.finish_test_button.clicked.connect(self.close)
         self.main_layout.addWidget(self.finish_test_button)
 
-        with open(cfg().config["path"] + '/Interface/css/Ending_test_widget.css', "r") as css:
-            self.setStyleSheet(css.read())
-
         self.show()
 
 
 if __name__ == '__main__':
     app = QApplication([])
-    test_widget = Ending_test_widget(result=5, wrong_answers=7, max_result=12)
+    test_widget = Ending_test_widget(result=1, max_result=12)
     app.exec_()
