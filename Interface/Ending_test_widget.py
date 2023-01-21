ff4d0654051eb3b4060d9b30ccfe26c5ccf46e9a
smasher <iluha_m_74@mail.ru>
2023-01-21 20:51:05 +0500

    Redesign of Ending_test_widget 1) Added progress bar 2) Window smaller 3) Wrong questions deleted 4) Css change for design

--- Interface/Ending_test_widget.py
+++ Interface/Ending_test_widget.py
@@ -1,13 +1,8 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QTabWidget, QVBoxLayout, QPushButton, QScrollArea, QHBoxLayout, QMainWindow, QLabel, QLineEdit
-from PyQt5.QtCore import QSize, Qt
-from test_data_funcs import get_all_tests, get_users
-from config import Config
-from db.sqlite import SQLInteract
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QProgressBar
+from config import Config as cfg
 
-class Ending_test_widget(QMainWindow):
-    """
-    Окно, которое вызывается после завершения теста
-    """
+
+class Ending_test_widget(QWidget):
     def __init__(self, result=0, wrong_answers=0, max_result=0, user={}, parent=None):
         super(QWidget, self).__init__(parent)
         self.result = result
@@ -17,51 +12,36 @@ class Ending_test_widget(QMainWindow):
         self.init_UI()
 
     def init_UI(self):
-        self.setMinimumSize(1280, 720)
+        self.setMinimumSize(400, 300)
 
-        with open(Config().config["path"] + '\\Interface\\css\\Ending_test_widget.css') as css:
-            self.css_file = css.read()
-            self.setStyleSheet(self.css_file)
-
-        self.main_widget = QWidget()
         self.main_layout = QVBoxLayout()
+        self.setLayout(self.main_layout)
 
-        self.test_finished_label = QLabel("Вы закончили тест")
-        self.main_layout.addWidget(self.test_finished_label)
+        # Create a horizontal layout for the result labels
+        result_layout = QHBoxLayout()
+        self.main_layout.addLayout(result_layout)
 
-        self.result_label = QLabel(f"Ваш результат: {self.result}/{self.max_result}")
-        self.main_layout.addWidget(self.result_label)
+        # Create a label for the test result
+        self.result_label = QLabel(f"Result: {self.result}/{self.max_result}")
+        result_layout.addWidget(self.result_label)
 
-        self.wrong_answers_label = QLabel(f"Количество неверно решённых заданий: {self.wrong_answers}")
-        self.main_layout.addWidget(self.wrong_answers_label)
+        # Create a progress bar to visualize the result
+        self.result_progress = QProgressBar()
+        self.result_progress.setValue(int((self.result / self.max_result) * 100))
+        result_layout.addWidget(self.result_progress)
 
-        self.finish_test_button = QPushButton("Закрыть")
-        self.finish_test_button.released.connect(self.finish_test_button_pushed)
+        # Create a button to close the widget
+        self.finish_test_button = QPushButton("Close")
+        self.finish_test_button.clicked.connect(self.close)
         self.main_layout.addWidget(self.finish_test_button)
 
+        with open(cfg().config["path"] + '/Interface/css/Ending_test_widget.css', "r") as css:
+            self.setStyleSheet(css.read())
 
-
-        self.main_widget.setLayout(self.main_layout)
-        self.setCentralWidget(self.main_widget)
         self.show()
-
-
-    def finish_test_button_pushed(self):
-        # test_db = SQLInteract(table_name='tests', filename_db=Config().config["path"] + '/db/users.db',
-        #                       values_of_this_table="(id, name, theme, max_result, questions)")
-        # print(1)
-        # test_arr = test_db.return_full_table(to_dict=True, element_for_transform="questions")
-        # print(test_arr)
-        # print(self.user)
-        self.close()
-        # from Interface.View_all_tests import View_all_tests
-        # self.view_all_tests = View_all_tests(tests=test_arr, user=self.user)
-
 
 
 if __name__ == '__main__':
     app = QApplication([])
-    solve_test = Ending_test_widget()
-
+    test_widget = Ending_test_widget(result=5, wrong_answers=7, max_result=12)
     app.exec_()
-
