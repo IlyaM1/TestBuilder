6c6ac33c2c4105eea377e45c6e4002e7b751c4ad
Catsher <iluha_m_74@mail.ru>
2023-07-07 12:19:34 +0500

    AdminPanelView refactor made all functions that should be private - private made some functions to generate widgets tried to do functions as small as it possible and nice

--- View/admin_panel_view.py
+++ View/admin_panel_view.py
@@ -25,87 +25,17 @@ class AdminPanelSignals(QtCore.QObject):
 class AdminPanelView(View):
     def __init__(self, users: list, tests: list):
         super().__init__(AdminPanelSignals())
-        self.set_interface_settings()
+        self.__set_interface_settings()
 
         self.tab_widget = QtWidgets.QTabWidget()
-        self.user_tab_layout = self.create_tab_widget(users, EntityType.USER)
-        self.test_tab_layout = self.create_tab_widget(tests, EntityType.TEST)
+        self.user_tab_layout = self.__create_tab_widget(users, EntityType.USER)
+        self.test_tab_layout = self.__create_tab_widget(tests, EntityType.TEST)
 
         container = QtWidgets.QVBoxLayout(self)
         container.addWidget(self.tab_widget)
 
-    def set_interface_settings(self):
-        self.setMinimumSize(1280, 720)
-        with open(Config.get_path() + '/View/css/admin_panel_view.css') as css:
-            self.setStyleSheet(css.read())
-
-    def create_tab_widget(self, entity_list: list, entity_type: EntityType):
-        """
-        Automatically adds itself in tab_widget
-        :param entity_list: list of entities (users or tests)
-        :param entity_type: type of entities (EntityType.TEST or EntityType.USER)
-        :return: layout that shows all entities from entity_list
-        """
-        entities_list_layout = QtWidgets.QVBoxLayout()
-
-        for entity in entity_list:
-            entity_object = Entity(entity["id"], entity["name"], entity_type)
-            entity_widget = self.create_entity_widget(entity_object)
-            entities_list_layout.addWidget(entity_widget)
-
-        button_entity_name = "тест" if entity_type == EntityType.TEST else "сотрудника"
-        new_entity_button = QtWidgets.QPushButton(f"Добавить {button_entity_name}")
-        new_entity_button.released.connect(lambda: self.new_entity_button_released(entity_type))
-
-        entities_list_widget = QtWidgets.QWidget()
-        entities_list_widget.setLayout(entities_list_layout)
-        scroll_widget = self.generate_scroll_widget(entities_list_widget)
-
-        final_widget_layout = QtWidgets.QVBoxLayout()
-        final_widget_layout.addWidget(scroll_widget)
-        final_widget_layout.addWidget(new_entity_button)
-
-        final_widget = QtWidgets.QWidget()
-        final_widget.setLayout(final_widget_layout)
-
-        tab_name = "Тесты" if entity_type == EntityType.TEST else "Сотрудники"
-        self.tab_widget.addTab(final_widget, tab_name)
-        return entities_list_layout
-
-    @staticmethod
-    def generate_scroll_widget(widget_to_set):
-        scroll_widget = QtWidgets.QScrollArea()
-
-        scroll_widget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
-        scroll_widget.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        scroll_widget.setWidgetResizable(True)
-        scroll_widget.setWidget(widget_to_set)
-
-        return scroll_widget
-
-    def create_entity_widget(self, entity):
-        widget = EntityWidget(entity)
-        widget.setFixedSize(self.width() - 60, 50)
-        widget.clicked.connect(lambda: self.entity_widget_clicked(entity))
-        widget.deleting.connect(lambda: self.entity_widget_deleting(entity))
-        return widget
-
-    def new_entity_button_released(self, entity_type):
-        self.signals.creating_clicked.emit(entity_type)
-
-    def entity_widget_clicked(self, entity):
-        self.signals.editing_clicked.emit(entity)
-
-    def entity_widget_deleting(self, entity):
-        self.signals.deleting_clicked.emit(entity)
-
-    def delete_entity_widget(self, entity):
-        layout = self.test_tab_layout if entity.type == EntityType.TEST else self.user_tab_layout
-        entity_widget = self.__find_widget_in_entity_layout(entity)
-        layout.removeWidget(entity_widget)
-
     def add_new_entity_widget(self, entity: Entity):
-        entity_widget = self.create_entity_widget(entity)
+        entity_widget = self.__create_entity_widget(entity)
         if entity.type == EntityType.TEST:
             self.test_tab_layout.addWidget(entity_widget)
         elif entity.type == EntityType.USER:
@@ -117,6 +47,81 @@ class AdminPanelView(View):
         entity_widget = self.__find_widget_in_entity_layout(entity)
         entity_widget.setText(entity.name)
 
+    def delete_entity_widget(self, entity):
+        layout = self.test_tab_layout if entity.type == EntityType.TEST else self.user_tab_layout
+        entity_widget = self.__find_widget_in_entity_layout(entity)
+        layout.removeWidget(entity_widget)
+
+    def __set_interface_settings(self):
+        self.setMinimumSize(1280, 720)
+        with open(Config.get_path() + '/View/css/admin_panel_view.css') as css:
+            self.setStyleSheet(css.read())
+
+    def __create_tab_widget(self, entity_list: list, entity_type: EntityType):
+        """
+        Automatically adds itself in tab_widget
+        :param entity_list: list of entities (users or tests)
+        :param entity_type: type of entities (EntityType.TEST or EntityType.USER)
+        :return: layout that shows all entities from entity_list
+        """
+
+        entities_layout = self.__generate_entities_layout(entity_list, entity_type)
+        entities_layout_wrapper = self.__create_wrapper_widget(entities_layout)
+        entities_scroll_widget = self.__generate_scroll_widget(entities_layout_wrapper)
+
+        new_entity_button = self.__generate_new_entity_button(entity_type)
+
+        final_widget_layout = QtWidgets.QVBoxLayout()
+        self.__layout_add_widgets(final_widget_layout, entities_scroll_widget, new_entity_button)
+        final_widget = self.__create_wrapper_widget(final_widget_layout)
+
+        tab_name = self.__get_tab_name(entity_type)
+        self.tab_widget.addTab(final_widget, tab_name)
+        return entities_layout
+
+    def __generate_entities_layout(self, entity_list: list, entity_type: EntityType):
+        entities_list_layout = QtWidgets.QVBoxLayout()
+
+        for entity in entity_list:
+            entity_object = Entity(entity["id"], entity["name"], entity_type)
+            entity_widget = self.__create_entity_widget(entity_object)
+            entities_list_layout.addWidget(entity_widget)
+
+        return entities_list_layout
+
+    def __generate_new_entity_button(self, entity_type: EntityType):
+        button_entity_name = self.__get_button_entity_name(entity_type)
+        new_entity_button = QtWidgets.QPushButton(f"Добавить {button_entity_name}")
+        new_entity_button.released.connect(lambda: self.__new_entity_button_released(entity_type))
+        return new_entity_button
+
+    @staticmethod
+    def __generate_scroll_widget(widget_to_set: QtWidgets.QWidget):
+        scroll_widget = QtWidgets.QScrollArea()
+
+        scroll_widget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        scroll_widget.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        scroll_widget.setWidgetResizable(True)
+        scroll_widget.setWidget(widget_to_set)
+
+        return scroll_widget
+
+    def __create_entity_widget(self, entity: Entity):
+        widget = EntityWidget(entity)
+        widget.setFixedSize(self.width() - 60, 50)
+        widget.clicked.connect(lambda: self.__entity_widget_clicked(entity))
+        widget.deleting.connect(lambda: self.__entity_widget_deleting(entity))
+        return widget
+
+    def __new_entity_button_released(self, entity_type: EntityType):
+        self.signals.creating_clicked.emit(entity_type)
+
+    def __entity_widget_clicked(self, entity: Entity):
+        self.signals.editing_clicked.emit(entity)
+
+    def __entity_widget_deleting(self, entity: Entity):
+        self.signals.deleting_clicked.emit(entity)
+
     def __find_widget_in_entity_layout(self, entity: Entity):
         layout = self.test_tab_layout if entity.type == EntityType.TEST else self.user_tab_layout
         for i in range(layout.count()):
@@ -124,6 +129,25 @@ class AdminPanelView(View):
             if widget.entity.id == entity.id:
                 return widget
         raise Exception("Can't find widget that contains this entity")
+
+    @staticmethod
+    def __layout_add_widgets(layout: QtWidgets.QLayout, *widgets: QtWidgets.QWidget):
+        for widget in widgets:
+            layout.addWidget(widget)
+
+    @staticmethod
+    def __create_wrapper_widget(layout: QtWidgets.QLayout):
+        wrapper_widget = QtWidgets.QWidget()
+        wrapper_widget.setLayout(layout)
+        return wrapper_widget
+
+    @staticmethod
+    def __get_button_entity_name(entity_type: EntityType):
+        return "тест" if entity_type == EntityType.TEST else "сотрудника"
+
+    @staticmethod
+    def __get_tab_name(entity_type: EntityType):
+        return "Тесты" if entity_type == EntityType.TEST else "Сотрудники"
 
 
 class EntityWidget(QtWidgets.QLabel):
