d92b4d0d341f7e0e9ec45119c209530441995af5
Catsher <iluha_m_74@mail.ru>
2023-07-11 00:06:35 +0500

    Refactored code: Now login func return only LoginStatus(Admin or User) User object should be gotten with other func (get_user_by_auth_info) Except return codes - raising Exceptions Also made __compare_auth_info_with_admin func to avoid 2-line if statement

--- Presenter/authorization_presenter.py
+++ Presenter/authorization_presenter.py
@@ -1,6 +1,7 @@
 from Presenter.presenter import Presenter
-from Model.authorization_model import LoginStatus
+from Model.authorization_model import LoginStatus, EmptyFieldLoginException, IncorrectAuthInfoLoginException
 from View.ui_utils import UiUtils
+
 
 class AuthorizationPresenter(Presenter):
     def __init__(self, view, model):
@@ -9,16 +10,16 @@ class AuthorizationPresenter(Presenter):
 
     def login(self):
         auth_info = self.view.get_auth_info()
-        login_status_array = self.model.login(auth_info)
-        status = login_status_array[0]
-        if status == LoginStatus.EMPTY_FIELDS:
+        try:
+            login_status = self.model.login(auth_info)
+        except EmptyFieldLoginException:
             UiUtils.call_error_window("Остались пустые поля ввода")
-        elif status == LoginStatus.INCORRECT_AUTH_INFO:
+        except IncorrectAuthInfoLoginException:
             UiUtils.call_error_window("Неправильное имя или пароль пользователя")
-        elif status == LoginStatus.ADMIN:
+        else:
             from window_manager import WindowManager
-            WindowManager.get_instance().open_admin_panel(self.model.get_users(), self.model.get_tests())
-        elif status == LoginStatus.USER:
-            from window_manager import WindowManager
-            user = login_status_array[1]
-            WindowManager.get_instance().open_test_selection_window(user, self.model.get_tests())
+            if login_status == LoginStatus.ADMIN:
+                WindowManager.get_instance().open_admin_panel(self.model.get_users(), self.model.get_tests())
+            elif login_status == LoginStatus.USER:
+                user = self.model.get_user_by_auth_info(auth_info)
+                WindowManager.get_instance().open_test_selection_window(user, self.model.get_tests())
