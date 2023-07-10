98ebac63177d9dfb974bb28d59a6a8d21b3d273d
Catsher <iluha_m_74@mail.ru>
2023-07-11 00:06:35 +0500

    Refactored code: Now login func return only LoginStatus(Admin or User) User object should be gotten with other func (get_user_by_auth_info) Except return codes - raising Exceptions Also made __compare_auth_info_with_admin func to avoid 2-line if statement

--- Model/authorization_model.py
+++ Model/authorization_model.py
@@ -2,14 +2,12 @@ from enum import Enum
 from db.sqlite import SQLInteract
 from db.auth_reg import Signing
 from config import Config
-from Model.entity import User, Test, Entity, EntityType
+# from Model.entity import User, Test, Entity, EntityType
 
 
 class LoginStatus(Enum):
     ADMIN = 1
     USER = 2
-    EMPTY_FIELDS = 3
-    INCORRECT_AUTH_INFO = 4
 
 
 class AuthInfo:
@@ -27,21 +25,28 @@ class AuthorizationModel:
     def login(self, auth_info: AuthInfo):
         """
         :param auth_info: AuthInfo object with name and password from inputs of auth widget
-        :return: Array, where first element LoginStatus and second element is user if LoginStatus==USER
+        :return: returns LoginStatus: Admin or User
         """
-
         if auth_info.name == '' or auth_info.password == '':
-            return [LoginStatus.EMPTY_FIELDS]
-
-        if auth_info.name == Config.get_instance().config["name"] \
-                and auth_info.password == Config.get_instance().config["password"]:
-            return [LoginStatus.ADMIN]
-
-        user = self.__authorization(auth_info)
-        if user is not False and user is not None:
-            return [LoginStatus.USER, user]
+            raise EmptyFieldLoginException()
+        elif self.__compare_auth_info_with_admin(auth_info):
+            return LoginStatus.ADMIN
         else:
-            return [LoginStatus.INCORRECT_AUTH_INFO]
+            # For person who will make authorization:
+            # Error return codes should be replaced with exception
+            # And there are shouldn't be "is not None" check, not sure about "is not False"
+            user = self.__authorization(auth_info)
+            if user is not False and user is not None:
+                return LoginStatus.USER
+            else:
+                raise IncorrectAuthInfoLoginException()
+
+    def get_user_by_auth_info(self, auth_info: AuthInfo):
+        """
+        Get user from database
+        :return: User object
+        """
+        pass
 
     def __authorization(self, auth_info: AuthInfo):
         sign_obj = Signing(auth_info, self.user_db)
@@ -60,3 +65,19 @@ class AuthorizationModel:
         :return: list of test's objects
         """
         pass
+
+    @staticmethod
+    def __compare_auth_info_with_admin(auth_info: AuthInfo):
+        is_name_equal = auth_info.name == Config.get_instance().config["name"]
+        is_password_equal = auth_info.password == Config.get_instance().config["password"]
+        return is_name_equal and is_password_equal
+
+
+class EmptyFieldLoginException(Exception):
+    def __init__(self):
+        super().__init__("Login is unsuccessful, because login and/or password inputs are empty")
+
+
+class IncorrectAuthInfoLoginException(Exception):
+    def __init__(self):
+        super().__init__("Login is unsuccessful, because login and/or password for admin/user are incorrect")
